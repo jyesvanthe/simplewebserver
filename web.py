4277b899d1f492a98b19fdb03798917b167f5f2f
jyesvanthe <jyesvanthev@gmail.com>
2024-03-13 15:43:13 +0530

    Success

--- web.py
+++ web.py
@@ -1,21 +1,51 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import HTTPServer,BaseHTTPRequestHandler
 content = """
 <!DOCTYPE html>
 <html>
-<head>
-<title>My webserver</title>
-</head>
-<body>
-<table border=2>
-<tr>
-<th>Company</th> <th>Revenue</th> <th>Financial Year</th>
-</tr>
-<tr>
-<td>Micro Soft</td> <td>$9034</td> <td>2014</td>
-</tr>
-</table>
-</body>
+     <title> Image Map </title>
+     <body>
+          <table border = "2" cellspacing = "10" cellpading = "6">
+               <caption> Top five revenue generating software companies</caption>
+               <tr>
+                    <th>Rank</th>
+                    <th>Company</th>
+                    <th>Revenue</th>
+                    <th>FY</th>
+               </tr>
+               <tr>
+                    <td>1</td>
+                    <td>Microsoft</td>
+                    <td>$86.8</td>
+                    <td>2014</td>
+               </tr>
+               <tr>
+                    <td>2</td>
+                    <td>Oracle</td>
+                    <td>$37.1</td>
+                    <td>2013</td>
+               </tr>
+               <tr>
+                    <td>3</td>
+                    <td>SAP</td>
+                    <td>$20.9</td>
+                    <td>2013</td>
+               </tr>
+               <tr>
+                    <td>4</td>
+                    <td>Symantec</td>
+                    <td>$6.8</td>
+                    <td>2013</td>
+               </tr>
+               <tr>
+                    <td>5</td>
+                    <td>VMware</td>
+                    <td>$5.2  </td>
+                    <td>2013</td>
+               </tr> 
+          </table>
+     </body>
 </html>
+
 """
 class myhandler(BaseHTTPRequestHandler):
     def do_GET(self):
